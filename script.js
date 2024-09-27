9087f2fd83756fd7ec2d8cdc29b6bb32b6b132ae
Esteban <esgonzalezor@cesde.net>
2024-09-26 20:17:33 -0500

    Implementacion de un recorrido a un arreglo de numeros

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let numero = 23
-let variasCosas = [23, true, 'Esteban', function(){}, [],  numero]
-let otroArreglo = []
+// let numero = 23
+// let variasCosas = [23, true, 'Esteban', function(){}, [],  numero]
+// let otroArreglo = []
  
-console.log(variasCosas[0]+ variasCosas[2])
-console.log (otroArreglo)
-console.log (variasCosas[0] * variasCosas[5])
+// console.log(variasCosas[0]+ variasCosas[2])
+// console.log (otroArreglo)
+// console.log (variasCosas[0] * variasCosas[5])
+
+let numeros = [1,2,3,4,5,6,7,8,9,10]
+for(let index = 0; index < numeros.length; index++){
+    // console.log(numeros)
+    if(numeros[index] % 2 == 0)
+        console.log (numeros[index])
+}
