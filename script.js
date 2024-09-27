e30b0e94bbe83597cf652afadd294438a7c02abb
Esteban <esgonzalezor@cesde.net>
2024-09-26 20:37:24 -0500

    Nuevo arreglo nombres

--- script.js
+++ script.js
@@ -6,9 +6,17 @@
 // console.log (otroArreglo)
 // console.log (variasCosas[0] * variasCosas[5])
 
-let numeros = [1,2,3,4,5,6,7,8,9,10]
-for(let index = 0; index < numeros.length; index++){
-    // console.log(numeros)
-    if(numeros[index] % 2 == 0)
-        console.log (numeros[index])
+// let numeros = [1,2,3,4,5,6,7,8,9,10]
+// for(let index = 0; index < numeros.length; index++){
+//     // console.log(numeros)
+//     if(numeros[index] % 2 == 0)
+//         console.log (numeros[index])
+// }
+
+let nombres =[]
+for (let index = 0; index < 5; index++){
+    nombres [index] = prompt('Ingrese su nombre: ')
+}
+for (let index = 0; index < nombres.length; index++){
+    console.log(nombres[index])
 }
